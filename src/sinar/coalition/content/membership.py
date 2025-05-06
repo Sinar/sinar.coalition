02806fcc5a9e77a35a97536a2457804f255bd5e8
Khairil Yusof <khairil.yusof@gmail.com>
2025-05-06 10:56:14 +0800

    Add initial membership type based on popolo-spec
    Coalition can add multiple Members with start and end dates.

--- src/sinar/coalition/content/membership.py
+++ src/sinar/coalition/content/membership.py
@@ -1,63 +1,84 @@
 # -*- coding: utf-8 -*-
 # from plone.app.textfield import RichText
-# from plone.autoform import directives
+from plone.autoform import directives
+# from collective import dexteritytextindexer
 from plone.dexterity.content import Container
+from plone.app.vocabularies.catalog import CatalogSource
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from z3c.relationfield.schema import RelationChoice
 from zope.interface import implementer
-
-
-# from sinar.coalition import _
+from sinar.coalition import _
 
 
 class IMembership(model.Schema):
     """ Marker interface and Dexterity Python Schema for Membership
     """
-    # If you want, you can load a xml model created TTW here
-    # and customize it in Python:
 
-    # model.load('membership.xml')
+    label = schema.TextLine(
+        title=_('Label'),
+        description=_("A label describing the membership eg. Member of SEA-ACN-Network"),
+        required=True,)
 
-    # directives.widget(level=RadioFieldWidget)
-    # level = schema.Choice(
-    #     title=_(u'Sponsoring Level'),
-    #     vocabulary=LevelVocabulary,
-    #     required=True
-    # )
+    role = schema.TextLine(
+        title=_('Role'),
+        description=_("A role that the member fulfills in, eg. Co-Chair of SEA-ACN"),
+        required=True,)
 
-    # text = RichText(
-    #     title=_(u'Text'),
-    #     required=False
-    # )
+    # Coalition
+    directives.widget('coalition', RelatedItemsFieldWidget,
+                      pattern_options={'basePath': '/',
+                                       'mode': 'auto',
+                                       'favourites': [], })
 
-    # url = schema.URI(
-    #     title=_(u'Link'),
-    #     required=False
-    # )
+    coalition = RelationChoice(title='Coalition',
+                               source=CatalogSource(portal_type='Coalition'),
+                               required=True,)
 
-    # fieldset('Images', fields=['logo', 'advertisement'])
-    # logo = namedfile.NamedBlobImage(
-    #     title=_(u'Logo'),
-    #     required=False,
-    # )
+    # Organization
+    directives.widget('organization', RelatedItemsFieldWidget,
+                      pattern_options={'basePath': '/',
+                                       'mode': 'auto',
+                                       'favourites': [], })
 
-    # advertisement = namedfile.NamedBlobImage(
-    #     title=_(u'Advertisement (Gold-sponsors and above)'),
-    #     required=False,
-    # )
+    organization = RelationChoice(title='Organization',
+                                  source=CatalogSource(portal_type='Organization'),
+                                  required=True,)
 
-    # directives.read_permission(notes='cmf.ManagePortal')
-    # directives.write_permission(notes='cmf.ManagePortal')
-    # notes = RichText(
-    #     title=_(u'Secret Notes (only for site-admins)'),
-    #     required=False
-    # )
+    start_date = schema.Date(
+        title=_('Start Date'),
+        description=_('Date which this relationship began'),
+        required=False,)
+
+    end_date = schema.Date(
+        title=_('End Date'),
+        description=_('Date which this relationship ended'),
+        required=False,)
 
 
 @implementer(IMembership)
 class Membership(Container):
     """ Content-type class for IMembership
     """
+
+    @property
+    def title(self):
+        ''' return label'''
+        return self.label
+
+    @title.setter
+    def title(self, value):
+        ''' we wont set a title here'''
+
+    @property
+    def description(self):
+        ''' return role'''
+        return self.role
+
+    @description.setter
+    def description(self, value):
+        ''' we wont set a title here'''
